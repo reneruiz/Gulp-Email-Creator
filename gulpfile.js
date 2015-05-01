2f2fe02ee34f35baf6c336bd15d6cf9c2318525a
Rene Ruiz <rruiz@sigmagroup.com>
2015-05-01 19:03:04 -0400

    unnecessary and selfish naming changes

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,9 @@
-// Include gulp
 var gulp = require('gulp');
 
-// Include Our Plugins
 var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
 var inline = require('gulp-mc-inline-css');
 var browserSync = require('browser-sync');
-var reload = browserSync.reload;
 var inlinesource = require('gulp-inline-source');
 var sendmail = require('gulp-mailgun');
 var util = require('gulp-util');
@@ -15,46 +12,42 @@ var fs = require('fs');
 // Include the config
 var config = require('./config.json');
 
-
 // Compile Our Sass
-gulp.task('sass', function() {
-    return gulp.src('src/scss/*.scss')
-    .pipe(sass({errLogToConsole: true}))
-    .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
-    .pipe(gulp.dest('src/css'))
-    .pipe(reload({stream:true}));
+gulp.task('css', function() {
+  return gulp.src('src/scss/*.scss')
+  .pipe(sass({errLogToConsole: true}))
+  .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
+  .pipe(gulp.dest('src/css'))
+  .pipe(browserSync.reload({stream:true}));
 });
 
 // BrowserSync
-gulp.task('browser-sync', function() {
-    browserSync({
-        server: {
-            baseDir: "./output",
-            index: "test-template.html"
-        },
-        open: "external",
-        logPrefix: "Gulp Email Creator"
-    });
+gulp.task('server', function() {
+  browserSync({
+    server: {
+      baseDir: './dist',
+      index: 'index.html'
+    },
+    open: "external",
+    logPrefix: "Gulp Email Creator"
+  });
 });
 
 // Build our templates
 gulp.task('build', function() {
-    return gulp.src('src/html/*.html')
-        .pipe(inlinesource())
-        .pipe(inline(config.APIKEY))
-        .pipe(gulp.dest('./output'))
-        .pipe(reload({stream:true}));
+  return gulp.src('src/html/*.html')
+    .pipe(inlinesource())
+    .pipe(inline(config.mailchimp.api_key))
+    .pipe(gulp.dest('./dist'))
+    .pipe(browserSync.reload({stream:true}));
 });
 
 // Watch Files For Changes
 gulp.task('watch', function() {
-    gulp.watch('src/scss/*.scss', ['sass']);
-    gulp.watch('src/html/*.html', ['build']);
-    gulp.watch('src/css/*.css', ['build']);
+  gulp.watch('src/scss/*.scss', ['sass']);
+  gulp.watch('src/html/*.html', ['build']);
+  gulp.watch('src/css/*.css', ['build']);
 });
-
-// Default Task
-gulp.task('default', ['sass', 'browser-sync', 'build', 'watch']);
 
 gulp.task('send', function () {
   gulp.src( 'dist/' + util.env.template)
@@ -65,6 +58,9 @@ gulp.task('send', function () {
     subject: 'This is a test email'
   }));
 });
+
+// Default Task
+gulp.task('default', ['css', 'server', 'build', 'watch']);
 
 gulp.task('litmus', function () {
     return sendEmail(util.env.template, config.litmus);
